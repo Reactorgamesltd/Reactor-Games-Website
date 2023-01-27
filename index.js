35a270bce84ab7968c488d8326d5aeb5800ee090
mariku-415 <adnan_malik415@hotmail.com>
2023-01-27 17:22:45 +0000

    doubled scrollbutton parameter

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ function leftScroll()
 {
     debugger;
     const left = document.querySelector(".card-game__cards-list");
-    left.scrollBy(-200,0);
+    left.scrollBy(-400,0);
 }
 
 function rightScroll()
 {
     const right = document.querySelector(".card-game__cards-list");
-    right.scrollBy(200,0);
+    right.scrollBy(400,0);
 }
